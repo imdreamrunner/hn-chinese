f00b02d5d6813e5f56124e5e13cc4f3afbb7f92a
Zhou Xinzi <imdreamrunner@gmail.com>
2015-04-27 12:27:56 +0800

    change db

--- store/_mysql.py
+++ store/_mysql.py
@@ -10,7 +10,7 @@ _db = None
 def get_cursor():
     global _db
     if _db is None:
-        _db = MySQLdb.connect("localhost","reader","reader","reader" )
+        _db = MySQLdb.connect("localhost", "hn-chinese", "reader", "reader" )
     return _db.cursor()
 
 
